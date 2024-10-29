f786ccf135df572eb19dfd9aea54de6ef9dae794
manoelvitorbernardino <00001095612906sp@al.educacao.sp.gov.br>
2024-10-29 09:55:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto e 2+2',
+    '4'
+)
+
+criaCartao(
+    'Técnologia e Robótica',
+    'Qual o comando que copia??',
+    'Ctrl c'
+)
+
+criaCartao(
+    'Matematica',
+    '3+2?',
+    '5'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz pessoas em ingles?',
+    'People!'
+)
